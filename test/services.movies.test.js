7e9a5e9fb9cb989018116aaa21349d8d61d7bb1c
Angel <angel.aven.c@gmail.com>
2021-06-10 17:17:54 -0500

    Clase 38 "Creación de test para nuestras utilidades"

---
+++ test/services.movies.test.js
@@ -0,0 +1,26 @@
+const assert = require('assert')
+const proxyquire = require('proxyquire')
+const { getAllStub, MongoLibMock } = require('../utils/mocks/mongoLib')
+const { moviesMock } = require('../utils/mocks/movies')
+
+describe('services - movies', function() {
+  const MoviesService = proxyquire('../services/movies', {
+    '../lib/mongo': MongoLibMock
+  })
+
+  const moviesService = new MoviesService()
+
+  describe('whe movies method is called', async function(){
+    it('Should call the getAll Mongolib method', async function () {
+      await moviesService.getMovies({})
+      assert.strictEqual(getAllStub.called, true)
+    })
+
+    it('Should return an array of movies', async function (){
+      const result = await moviesService.getMovies({})
+      const expected = moviesMock
+      console.log('------------------->', result)
+      assert.deepStrictEqual(result, expected)
+    })
+  })
+})
